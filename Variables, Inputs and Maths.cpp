edde797009dbb3ee955cdbe5e57ff5456c996f31
Soup <87945079+Numb11@users.noreply.github.com>
2021-12-22 22:36:36 +0000

    Create Variables, Inputs and Maths.cpp

---
+++ Variables, Inputs and Maths.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+    int wholenum = 1; //An integer
+    float floatnum = 1.32; //Floaitng point number
+    double doublenum = 1.50; //Floating point number
+    bool boolean = true; //Boolean one or two values, True or False
+    string text = "Hello"; //String(concatenation of characters)
+    const int num = 10; //Constant declaration(Read Only)
+    int x = 1, y = 2, z = 3; // Declaration of multiple variables
+    string message; // Variable declaration
+    cout << "This is an example of an output!\n";
+    cout << "Break lines can be acheived with /n\n"; //Output with Break Line
+    cout << "Enter soemthing:";
+    cin >> message; //Input
+    cout << "This is your message:" << message; //Display message and variable
+    return 0;
+
+}
